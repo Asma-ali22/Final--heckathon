2dd48ab1d5a58068c838e09295a44e3abf4fc822
Asma-ali22 <160036743+Asma-ali22@users.noreply.github.com>
2024-12-09 20:51:22 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Editor from "./components/editor";
+import Products from "./components/products";
+import Classic from "./components/classic";
+import Universe from "./components/universe";
+import Featured from "./components/featured";
+import Footer from "./components/footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+      <Editor/>
+      <Products/>
+      <Classic/>
+      <Universe/>
+      <Featured/>
+      <Footer/>
+    </div>
+  );
+}
